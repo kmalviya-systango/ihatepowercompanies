7b9b3fa8267427e9147d8cbfd440fef33c39d158
kmalviya_isystango <kmalviya@isystango.com>
2017-06-30 19:22:03 +0530

    Uploading file with ihpc comments plugins

--- wp-content/themes/ihpc/functions.php
+++ wp-content/themes/ihpc/functions.php
@@ -1597,3 +1597,52 @@ function get_review_meta($review_id){
 	$returnArray['losses']['So I want'] = get_post_meta($review_id,'_want',true);	
 	return $returnArray;
 }
+
+/***
+* Getting comment in ihpc format
+****/
+function ihpc_comment($comment, $args, $depth){
+	if ( 'div' === $args['style'] ) {
+        $tag       = 'div';
+        $add_below = 'comment';
+    } else {
+        $tag       = 'li';
+        $add_below = 'div-comment';
+    }
+    ?>
+    <<?php echo $tag ?> <?php comment_class( empty( $args['has_children'] ) ? '' : 'parent' ) ?> id="comment-<?php comment_ID() ?>">
+    <?php if ( 'div' != $args['style'] ) : ?>
+        <div id="div-comment-<?php comment_ID() ?>" class="comment-body">
+    <?php endif; ?>
+    <div class="comment-meta commentmetadata"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ); ?>">
+        <?php
+        printf( __( '<cite class="fn">%s</cite>' ), get_comment_author_link() ); 
+        printf( __('%1$s at %2$s'), get_comment_date(),  get_comment_time() ); ?></a><?php edit_comment_link( __( '(Edit)' ), '  ', '' );
+        ?>
+    </div>
+    <div class="comment-author vcard">
+        <div class="comment_author_image">
+        	<?php
+        	$authorID = get_comment_author( $comment->comment_ID );
+        	$pro_pic  = get_field('user_profile_pic','user_'.$authorID);
+			if( !empty($pro_pic) ){				
+				echo "<img src='$pro_pic' class='avatar avatar-128 photo avatar-default' height='128' width='128' />";
+			}
+			else if( $args['avatar_size'] != 0 ){
+				echo get_avatar( $comment, $args['avatar_size'] );
+			}
+    		?>
+        </div>
+        <?php if ( $comment->comment_approved == '0' ) : ?>
+	        <div><em class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.' ); ?></em></div>
+	    <?php endif; ?>  
+	    <div class="comment_body"><?php comment_text(); ?></div>
+    </div>    
+    <div class="reply">
+        <?php comment_reply_link( array_merge( $args, array( 'add_below' => $add_below, 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
+    </div>
+    <?php if ( 'div' != $args['style'] ) : ?>
+    </div>
+    <?php endif; ?>
+    <?php
+}
